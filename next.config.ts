147a3b96925c70265c479907ce2819e21fe39716
Arsalan Ali Khattak <to.arsalanali@gmail.com>
2026-01-03 02:46:59 -0800

    Update Next.js to 16.1.1 and fix config for deployment

--- next.config.ts
+++ next.config.ts
@@ -11,12 +11,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
 };
 
 export default nextConfig;
